b8bcc9f1e97fa48d5a51c165e3a903e5831bd864
Cynthia Ponce and Victor Guttmann <victor@>
2019-08-13 13:49:14 -0700

    Add buttons using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
+  });
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+});
